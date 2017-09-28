4d752ec99fff546aabbbb87a63be75283795195c
David Mulder <dmulder@suse.com>
2017-09-28 07:25:49 -0600

    Add the initial source files

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yast-py-buffer',
+      version='0.1',
+      description='A buffer layer between the yast-python-bindings and the module source',
+      author='David Mulder',
+      author_email='dmulder@suse.com',
+      url='https://github.com/dmulder/yast-py-buffer',
+      packages=['yui'],
+)
